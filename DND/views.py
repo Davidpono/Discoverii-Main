b9a632b48c2e0348b06b82fb88a0c8a90413ee0a
David Ponce <107604928+Davidpono@users.noreply.github.com>
2024-02-01 12:55:58 -0500

    apiupdate

--- DND/views.py
+++ DND/views.py
@@ -10,7 +10,7 @@ import requests
 from openai import OpenAI
 
 load_dotenv()
-api_key = os.getenv("OPENAI_KEY", None)
+api_key = os.getenv("OPENAI_API_KEY", None)
 client = OpenAI(api_key=api_key)
 def dnd_view(request):
     chatbot_response = None
